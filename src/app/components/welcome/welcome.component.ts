81e2d6166616d9050f0db94191c93ff34d65cdd5
TvojaMami <nejkojager@gmail.com>
2024-07-12 14:17:51 +0200

    UI polish swedish

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
-
+  mottos = [
+    "An app where you avoid boring drinking 4 life",
+    "Sip Smartly, Live Lively",
+    "Shake Up Your Drink Routine",
+    "Discover Your Next Favorite Cocktail",
+    "Every Drink, a New Adventure",
+    "Mix, Sip, Enjoy",
+    "Raise the Bar on Happy Hour",
+    "Craft Your Perfect Cocktail",
+    "Find Flavor, Find Fun",
+    "Cocktail Inspiration at Your Fingertips",
+    "Cheers to New Discoveries"
+  ];
+  motto: string = ""
+  
+  giveRandomMoto () {
+    return this.mottos[Math.floor(Math.random() * this.mottos.length)];
+  }
+  
+  ngOnInit(): void {
+    this.motto = this.giveRandomMoto()
+  }
 }
